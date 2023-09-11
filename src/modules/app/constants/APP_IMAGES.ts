a7bf91678fcd4930be00fffedd089d105505a2b6
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-11 12:12:54 -0400

    Update images

--- src/modules/app/constants/APP_IMAGES.ts
+++ src/modules/app/constants/APP_IMAGES.ts
@@ -1,8 +1,8 @@
 export const APP_IMAGES = {
   ME_IMAGE: { image: '/images/me.png', alt: 'chacaponquin_avatar' },
-  CHACA_NPM_IMAGE: { image: '/images/chaca_npm.png', alt: 'chaca_image' },
-  CHACA_WEB_IMAGE: { image: '/images/chaca_web.png', alt: 'chaca_web_image' },
-  CRYPTO_IMAGE: { image: '/images/crypto.png', alt: 'ch_crypto_app' },
-  CHACA_DOCS_IMAGE: { image: '/images/chaca-doc.png', alt: 'chaca-docs' },
+  CHACA_NPM_IMAGE: { image: '/images/chaca_npm.webp', alt: 'chaca_image' },
+  CHACA_WEB_IMAGE: { image: '/images/chaca_web.webp', alt: 'chaca_web_image' },
+  CRYPTO_IMAGE: { image: '/images/crypto.webp', alt: 'ch_crypto_app' },
+  CHACA_DOCS_IMAGE: { image: '/images/chaca-doc.webp', alt: 'chaca-docs' },
   LOGO: { image: '/logo/portfolio_logo.png', alt: 'hector_portfolio_logo' },
 } as const;
